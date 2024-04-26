1d0359a289c49335978a40b293c13579e70f6624
devang-yagnik <devangyagnik865@gmail.com>
2024-04-26 15:31:02 +0530

    initial commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-inherit">
+      <p className="text-lg text-black">Customers Page !</p>
+    </div>
+  );
+}
